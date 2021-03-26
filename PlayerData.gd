eb7b58f9e17a1056294076d60702deae981742da
ilipovcev <ilya.lipovcev@bk.ru>
2021-03-27 02:22:29 +0300

    проверка соединений

---
+++ PlayerData.gd
@@ -0,0 +1,6 @@
+extends Node
+
+class_name PlayerData
+
+export(int) var peer_id
+export(String) var nick
--- Server.gd
+++ Server.gd
@@ -1,9 +1,14 @@
 extends Node
- 
+
+
+var PlayerData : = preload("res://PlayerData.gd") as Script;
+tool
+
 var network = NetworkedMultiplayerENet.new()
 const SERVER_PORT = 1909
 const MAX_PLAYERS = 4
 
+var players: Array;
 
 func _ready():
 	startServer()
@@ -19,8 +24,19 @@ func startServer():
 
 
 func _Peer_Connected(id):
-	print("User ", id, " connected") 
-	
+	print("User ", id, " connected")
+
 	
 func _Peer_Disconnected(id):
-	print("User ", id, " disconnected") 
+	print("User ", id, " disconnected")
+
+
+remote func RegPlayer(name):
+	var idPlayer = get_tree().get_rpc_sender_id()
+	var player = PlayerData.new();
+	player.peer_id = idPlayer;
+	player.nick = name;
+	players.push_front(player);
+	var playerStr = [idPlayer, name]
+	rpc("OnRegPlayer", playerStr);
+	
